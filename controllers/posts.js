fc9d32bd91449a34c930f2569863afd1e56f9c72
renjithspace <renjithrajeevvk@gmail.com>
2018-10-22 19:52:35 +0530

    Update get with more posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,13 +1,22 @@
 module.exports = {
   get(req, res) {
-    const data = {
-      title: 'Sample post',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
-      tags: 'sample, lorem, ipsum',
-      author: 'John Deo',
-      created: '22/10/2018'
-    }
+    const posts = [
+      {
+        title: 'Sample post',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+        tags: 'sample, lorem, ipsum',
+        author: 'John Deo',
+        created: '22/10/2018'
+      },
+      {
+        title: 'Another sample post',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+        tags: 'another, sample, lorem, ipsum',
+        author: 'Angela Deo',
+        created: '23/10/2018'
+      }
+    ]
 
-    res.json(data)
+    res.json(posts)
   }
 }
