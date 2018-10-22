c3212a002412ba02b2e5ecbe5cffa784deb815e2
renjithspace <renjithrajeevvk@gmail.com>
2018-10-22 19:27:26 +0530

    Import port and hostname from config

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const config = require('./server.config')
 const routes = require('./routes')
 
 const app = express()
+const hostname = config.hostname
+const port = config.port
+
 routes.init(app)
 
-app.listen(8000, () => console.log('Server run on port 8000'))
+app.listen(port, hostname, () => console.log(`Server running on http://${hostname}:${port}`))
