333aef88effd02bc013b3d71e460d873fcfe4a6b
renjithspace <renjithrajeevvk@gmail.com>
2018-10-22 19:55:16 +0530

    Fix routes import path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const config = require('./server.config')
-const routes = require('./routes')
+const routes = require('./http/routes')
 
 const app = express()
 const hostname = config.hostname
